057b61bf206deb9efea4b59e670180b88a801ba0
1DrTimE1 <drtime171@gmail.com>
2025-11-13 12:21:32 +0100

    fxmanifest.lua Updated v0.1.3

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'author'
+description 'Noclip-Fivem'
+version '1.1.0'
+
+shared_scripts {
+    -- '@ox_lib/init.lua' 
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+state_bags 'player'
